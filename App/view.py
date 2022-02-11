7882053e4f830d6a58b39ff6564f4c25eaa479b2
s.tenjov@uniandes.edu.co <s.tenjov@uniandes.edu.co>
2022-02-10 21:36:15 -0500

    Cambios Estudiante 2

--- App/view.py
+++ App/view.py
@@ -83,7 +83,7 @@ while True:
 
 
         # TODO: Modificación de Est-2 en el Lab 2
-        print('Último libro cargado: ' + str(lt.lastElement(books)))
+        
 
     elif int(inputs[0]) == 2:
         print("Cargando información de tags....")
@@ -91,7 +91,10 @@ while True:
         print('Total de tags cargados: ' + str(lt.size(tags)))
     elif True:
         # TODO: Modificación de Est-2 en el Lab 2
-        pass
+        elif int(inputs[0]) == 3:
+            print("Cargando información de Book-Tags...")
+                                booktags = loadBookTags()
+            print('Total de Book-Tags cargados: ' + str(lt.size(booktags)))
 
     else:
         sys.exit(0)
